03391de51eaa86e2d9a09a2824b73ee79b4e7bb0
konstantin664 <konstantin-hig@web.de>
2021-01-07 20:40:09 +0100

    weird

---
+++ Betriebssysteme/Abgabe/B3/Vorlage/cmake-build-debug/CMakeFiles/scheduler.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scheduler.dir/src/Scheduler.cpp.obj"
+  "CMakeFiles/scheduler.dir/src/Task.cpp.obj"
+  "CMakeFiles/scheduler.dir/src/main.cpp.obj"
+  "libscheduler.dll.a"
+  "scheduler.exe"
+  "scheduler.exe.manifest"
+  "scheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
